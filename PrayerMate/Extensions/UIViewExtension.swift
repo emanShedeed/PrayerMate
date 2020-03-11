51b4ae6b808e0e890f541c4a10f05a90344699b6
BSS <eman.shedeed@black-squares.com>
2020-03-11 17:59:34 +0200

    set splach background under status bar

---
+++ PrayerMate/Extensions/UIViewExtension.swift
@@ -0,0 +1,59 @@
+//
+//  UIViewExtension.swift
+//  Ataba
+//
+//  Created by eman shedeed on 8/22/19.
+//  Copyright © 2019 eman shedeed. All rights reserved.
+//
+
+import UIKit
+typealias GradientPoints = (startPoint: CGPoint, endPoint: CGPoint)
+//set the start and the end of graiant
+enum GradientOrientation {
+    case topRightBottomLeft
+    case topLeftBottomRight
+    case horizontal
+    case vertical
+    
+    var startPoint : CGPoint {
+        return points.startPoint
+    }
+    
+    var endPoint : CGPoint {
+        return points.endPoint
+    }
+    
+    var points : GradientPoints {
+        switch self {
+        case .topRightBottomLeft:
+            return (CGPoint(x: 0.0,y: 1.0), CGPoint(x: 1.0,y: 0.0))
+        case .topLeftBottomRight:
+            return (CGPoint(x: 0.0,y: 0.0), CGPoint(x: 1,y: 1))
+        case .horizontal:
+            return (CGPoint(x: 0.0,y: 0.5), CGPoint(x: 1.0,y: 0.5))
+        case .vertical:
+            return (CGPoint(x: 0.0,y: 0.0), CGPoint(x: 0.0,y: 1.0))
+        }
+    }
+}
+@IBDesignable
+extension UIView {
+    //to make button color between two colors
+    func applyGradient(with colours: [UIColor], locations: [NSNumber]? = nil) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+    
+    func applyGradient(with colours: [UIColor], gradient orientation: GradientOrientation) {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.startPoint = orientation.startPoint
+        gradient.endPoint = orientation.endPoint
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+    
+}
